db16075576b9437a67cf50b623a3f1f336caa6b0
ju4700 <jalal.dev.design@gmail.com>
2025-03-12 17:22:06 +0600

    Remove client Tasks component and related type definitions

--- client/src/utils/fetchWithTimeout.ts
+++
@@ -1,7 +0,0 @@
-export const fetchWithTimeout = async (url: string, options: RequestInit = {}, timeout = 5000): Promise<Response> => {
-    const controller = new AbortController();
-    const id = setTimeout(() => controller.abort(), timeout);
-    const response = await fetch(url, { ...options, signal: controller.signal });
-    clearTimeout(id);
-    return response;
-  };
