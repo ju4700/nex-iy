a908640494171b8eb988441328037cd1fa6d2c67
ju4700 <jalal.dev.design@gmail.com>
2025-03-13 16:05:34 +0600

    Remove unused files and configurations from the server

--- nexify-server/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { UserModel } from './user';
-export { TeamModel } from './team';
-export { MessageModel } from './message';
-export { TaskModel } from './task';
-export { FileModel } from './file';
